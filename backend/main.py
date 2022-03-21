ec6b2559acd27473b8c0a2cb3e3f03314dc7f54b
maxcore25 <mkorobov2002@mail.ru>
2022-03-21 15:54:50 +0300

    FastAPI Execute Function Added

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,4 @@
 import jwt
-
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from passlib.hash import bcrypt
@@ -8,7 +7,16 @@ from tortoise import fields
 from tortoise.contrib.fastapi import register_tortoise
 from tortoise.contrib.pydantic import pydantic_model_creator
 from tortoise.models import Model
+from typing import Optional
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 JWT_SECRET = 'myjwtsecret'
 
 app = FastAPI()
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8000, host='127.0.0.1')
+
+    # Or use terminal command: uvicorn main:app --reload
