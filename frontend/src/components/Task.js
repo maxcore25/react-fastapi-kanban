a4d18fff00d4cfa71e2895c7f8220fc5cd98a034
maxcore25 <mkorobov2002@mail.ru>
2022-03-23 05:59:58 +0300

    Task Styling Added

--- frontend/src/components/Task.js
+++ frontend/src/components/Task.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Draggable } from 'react-beautiful-dnd';
+import styled from 'styled-components';
 
-export default function Task() {
+const Container = styled.div`
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  padding: 8px;
+  margin-bottom: 8px;
+  background-color: white;
+`;
+
+export default function Task(props) {
   return <div>Task</div>;
 }
