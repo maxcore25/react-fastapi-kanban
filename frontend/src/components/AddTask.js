182b2f0569908ce197addca62b7e1f3407e6d540
maxcore25 <mkorobov2002@mail.ru>
2022-03-27 15:18:28 +0300

    handleInputChange Function Written

--- frontend/src/components/AddTask.js
+++ frontend/src/components/AddTask.js
@@ -4,12 +4,28 @@ export default function AddTask(props) {
   const [showNewTaskButton, setShowNewTaskButton] = useState(true);
   const [value, setValue] = useState('');
 
+  function handleInputChange(e) {
+    setShowNewTaskButton(true);
+    addNewTask(props.columnId, value);
+    setValue('');
+  }
+
+  function addNewTask(columnId, content) {
+    const newTaskId = 'task-' + Math.floor(Math.random() * 100000);
+    const column = props.board.column[columnId];
+  }
+
   return (
     <div>
-      {setShowNewTaskButton ? (
-        <button>New</button>
+      {showNewTaskButton ? (
+        <button onClick={() => setShowNewTaskButton(false)}>New</button>
       ) : (
-        <input type='text' value={value} />
+        <input
+          type='text'
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          onBlur={handleInputChange}
+        />
       )}
     </div>
   );
