30110c8aa6f7f3899585c66940bdffe124b24524
maxcore25 <mkorobov2002@mail.ru>
2022-03-29 10:59:56 +0300

    Pydantic Schema Created

--- backend/main.py
+++ backend/main.py
@@ -16,6 +16,32 @@ JWT_SECRET = 'myjwtsecret'
 
 app = FastAPI()
 
+
+class Task(BaseModel):
+    id: str
+    content: str
+
+
+class Tasks(BaseModel):
+    __root__: dict[str, Task]
+
+
+class Column(BaseModel):
+    id: str
+    title: str
+    taskIds: list
+
+
+class Columns(BaseModel):
+    __root__: dict[str, Task]
+
+
+class Board(BaseModel):
+    tasks: Tasks
+    columns: Columns
+    columnOrder: list
+
+
 origins = ["*"]
 
 app.add_middleware(
