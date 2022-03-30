0dad163235b2948b3dd4278e0876825f6aeda212
maxcore25 <mkorobov2002@mail.ru>
2022-03-30 12:20:08 +0300

    Lesson Code Copied

--- backend/main.py
+++ backend/main.py
@@ -33,7 +33,7 @@ class Column(BaseModel):
 
 
 class Columns(BaseModel):
-    __root__: dict[str, Task]
+    __root__: dict[str, Column]
 
 
 class Board(BaseModel):
@@ -45,52 +45,83 @@ class Board(BaseModel):
 class User(Model):
     id = fields.IntField(pk=True)
     username = fields.CharField(50, unique=True)
-    password = fields.CharField(200)
-    board = fields.JSONField(default={'tasks': {}, 'columns': {}, 'columnOrder': []})
+    password_hash = fields.CharField(128)
+    board_data = fields.JSONField(default='{"tasks": {}, "columns": {}, "columnOrder": []}')
+
+    def verify_password(self, password):
+        return bcrypt.verify(password, self.password_hash)
 
 
 User_Pydantic = pydantic_model_creator(User, name='User')
-UserIn_Pydantic = pydantic_model_creator(User, name='UserIn', exclude_readonly=True, exclude=('board',))
+UserIn_Pydantic = pydantic_model_creator(User, name='UserIn', exclude_readonly=True, exclude=('board_data',))
 
-origins = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=origins,
-    allow_headers=origins,
-)
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
 
-@app.get('/board')
-def get_board():
-    board_data = {
-        'tasks': {
-            'task-1': {'id': 'task-1', 'content': 'go to the shop'},
-            'task-2': {'id': 'task-2', 'content': 'buy some milk'},
-            'task-3': {'id': 'task-3', 'content': 'come back home'},
-        },
-        'columns': {
-            'column-1': {
-                'id': 'column-1',
-                'title': 'To do',
-                'taskIds': ['task-2', 'task-3']
-            },
-            'column-2': {
-                'id': 'column-2',
-                'title': 'Done',
-                'taskIds': ['task-1', ]
-            },
-        },
-        'columnOrder': ['column-1', 'column-2'],
-    }
-    return {'board': board_data}
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
+        user = await User.get(id=payload.get('id'))
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid username or password'
+        )
+
+    return await User_Pydantic.from_tortoise_orm(user)
+
+
+@app.get("/board")
+async def get_board(user: User_Pydantic = Depends(get_current_user)):
+    return {'board': user.board_data}
+
+
+@app.post("/board")
+async def save_board(board: Board, user: User_Pydantic = Depends(get_current_user)):
+    user = await User.get(id=user.id)
+    user.board_data = board.json()
+    await user.save()
+
+    return {"status": "success"}
+
+
+@app.post('/users')
+async def create_user(user: UserIn_Pydantic):
+    user_obj = User(username=user.username, password_hash=bcrypt.hash(user.password_hash))
+    await user_obj.save()
+    user_obj_py = await User_Pydantic.from_tortoise_orm(user_obj)
+    token = jwt.encode(user_obj_py.dict(), JWT_SECRET)
+    return {'access_token': token, 'token_type': 'bearer'}
+
+
+async def authenticate_user(username: str, password: str):
+    user = await User.get(username=username)
+    if not user:
+        return False
+    if not user.verify_password(password):
+        return False
+    return user
+
+
+@app.post('/token')
+async def generate_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = await authenticate_user(form_data.username, form_data.password)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid username or password'
+        )
+
+    user_obj = await User_Pydantic.from_tortoise_orm(user)
+    token = jwt.encode(user_obj.dict(), JWT_SECRET)
+
+    return {'access_token': token, 'token_type': 'bearer'}
 
 
 register_tortoise(
     app,
-    db_url='postgres://postgres:Password1@localhost:5432/postgres',
+    db_url='postgres://postgres:Password1@127.0.0.1:5432/postgres',
     modules={'models': ['main']},
     generate_schemas=True,
     add_exception_handlers=True
@@ -99,4 +130,4 @@ register_tortoise(
 if __name__ == '__main__':
     uvicorn.run(app, port=8000, host='127.0.0.1')
 
-    # Or use terminal command: uvicorn main:app --reload
+# Or use terminal command: uvicorn main:app --reload
